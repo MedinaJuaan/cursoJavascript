722b205b024c7e4dd1879a9093e43fafc03972fb
MedinaJuaan <114200841+MedinaJuaan@users.noreply.github.com>
2023-02-01 00:11:29 -0300

    Entrega final Medina Juan

--- js/cotizador.js
+++ js/cotizador.js
@@ -1,106 +1,131 @@
-let stockProductos = [
-    {id: 1, nombre: "Kit Basico CCTV", tipo: "alarma", cantidad: 1, desc: "Kit compuesto por: DVR4ch,2camaras y disco de 1TB", precio: 60000, img: './Imagenes/Cctv/KitCCTV.webp'},
-    {id: 2, nombre: "Dvr4ch", tipo: "cctv", cantidad: 1, desc: "Dvr 4 canales", precio: 35000, img: './Imagenes/Cctv/Dvr_4ch.jpeg'},
-    {id: 3, nombre: "Dvr8ch", tipo: "cctv", cantidad: 1, desc: "Dvr 8 canales", precio: 45000, img: './Imagenes/Cctv/Dvr_8ch.jpeg'},
-    {id: 4, nombre: "Dvr16ch", tipo: "cctv", cantidad: 1, desc: "Dvr 16 canales", precio: 65000, img: './Imagenes/Cctv/Dvr_16ch.jpg'},
-    {id: 5, nombre: "Camara", tipo: "cctv", cantidad: 1, desc: "Camara 12v", precio: 9000, img: './Imagenes/Cctv/Camara.jpg'},
-    {id: 6, nombre: "Disco 1TB", tipo: "cctv", cantidad: 1, desc: "Disco 1TB de capacidad", precio: 17000, img: './Imagenes/Cctv/Disco1TB.jpg'},
-    {id: 7, nombre: "Disco 2TB", tipo: "cctv", cantidad: 1, desc: "Disco 2TB de capacidad", precio: 23000, img: './Imagenes/Cctv/Disco2TB.jpg'},
-    {id: 13, nombre: "Kit Basico Alarma", tipo: "alarma", cantidad: 1, desc: "Kit compuesto por: Central,teclado,2 sensores de movimiento interior, 1 magnetico y sirena interior", precio: 62000, img: './Imagenes/Alarma/KitAlarma.webp'},
-    {id: 8, nombre: "Central de alarma", tipo: "alarma", cantidad: 1, desc: "Central de alarma domiciliaria", precio: 35000, img: './Imagenes/Alarma/CentralAlarma.jpg'},
-    {id: 9, nombre: "Teclado", tipo: "alarma", cantidad: 1, desc: "Teclado de activacion", precio: 17000, img: './Imagenes/Alarma/Teclado.webp'},
-    {id: 10, nombre: "Sensor interior", tipo: "alarma", cantidad: 1, desc: "Sensor de movimiento interior", precio: 7000, img: './Imagenes/Alarma/PirInterior.jpg'},
-    {id: 11, nombre: "Sensor exterior", tipo: "alarma", cantidad: 1, desc: "Sensor de movimiento exterior", precio: 15000, img: './Imagenes/Alarma/PirExterior.jpg'},
-    {id: 12, nombre: "Sensor magnetico", tipo: "alarma", cantidad: 1,desc: "Sensor magnetico", precio: 1000, img: './Imagenes/Alarma/Magnetico.webp'},
-    {id: 14, nombre: "Sirena interior", tipo: "alarma", cantidad: 1, desc: "Sirena interior", precio: 8000, img: './Imagenes/Alarma/SirenaInterior.jpg'},
-    {id: 15, nombre: "Sirena exterior", tipo: "alarma", cantidad: 1, desc: "Sirena exterior", precio: 7000, img: './Imagenes/Alarma/SirenaExterior.jpg'},
-    {id: 16, nombre: "Barrera", tipo: "alarma", cantidad: 1, desc: "Barrera exterior hasta 40mts", precio: 13000, img: './Imagenes/Alarma/Barrera.webp'},
-];
-
-const contenedorProductos = document.getElementById('contenedor-productos');
-const contenedorCarrito = document.getElementById('carrito-contenedor');
-const botonVaciar = document.getElementById('vaciar-carrito');
-const contadorCarrito = document.getElementById('contadorCarrito');
-const cantidad = document.getElementById('cantidad');
-const precioTotal = document.getElementById('cotizacionTotal');
-const cantidadTotal = document.getElementById('cantidadTotal');
-
 let carrito = [];
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const procesarCompra = document.querySelector("#procesarCompra");
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        actualizarCarrito();
-    }
-});
-
-botonVaciar.addEventListener('click', () => {
-    carrito.length = 0
-    actualizarCarrito();
-});
-
-stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <img src=${producto.img} alt= "">
-    <h3>${producto.nombre}</h3>
-    <p>${producto.desc}</p>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-    `
-    contenedorProductos.appendChild(div)
-    const boton = document.getElementById(`agregar${producto.id}`)
- 
-
-    boton.addEventListener('click', () => {
-        agregarAlCarrito(producto.id);
+fetch("./data/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((prod) => {
+      const { id, nombre, desc, img, cantidad } = prod;
+      if (contenedor) {
+        contenedor.innerHTML += `
+    <div class="card mt-3" style="width: 18rem;">
+    <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">Descripcion: ${desc}</p>
+      <p class="card-text">Cantidad: ${cantidad}</p>
+      <button class="btn btn-primary" onclick="agregarProducto(${id})">Cotizar Producto</button>
+    </div>
+  </div>
+    `;
+      }
     });
+  });
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  mostrarCarrito();
 });
 
-const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some (prod => prod.id === prodId) 
-    if (existe){
-        const prod = carrito.map (prod => { 
-            if (prod.id === prodId){
-                prod.cantidad++
-            }
-        })
-    } else { 
-        const item = stockProductos.find((prod) => prod.id === prodId)
-        carrito.push(item)
-    }
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
+    mostrarCarrito();
+  });
+}
 
-    actualizarCarrito() ;
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "¡Tu carrito está vacio!",
+        text: "Agrega productos para continuar con la cotizacion",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    } else {
+      const total = carrito.reduce(
+        (acc, prod) => acc + prod.cantidad * prod.precio,
+        0
+      );
+      Swal.fire({
+        title: "Gracias por elegirnos",
+        text: `Tu cotizacion total es de $${total}`,
+        icon: "success",
+        confirmButtonText: "Aceptar",
+      });
+    }
+    setTimeout(() => {
+      localStorage.removeItem("carrito");
+      location.reload();
+    }, 5000);
+  });
+}
+
+let data = [];
+
+fetch("./data/data.json")
+  .then((res) => res.json())
+  .then((productos) => {
+    data = productos;
+  });
+const agregarProducto = (id) => {
+  const existe = carrito.some((prod) => prod.id === id);
+
+  if (existe) {
+    const prod = carrito.map((prod) => {
+      if (prod.id === id) {
+        prod.cantidad++;
+      }
+    });
+  } else {
+    const item = data.find((prod) => prod.id === id);
+    carrito.push(item);
+  }
+  mostrarCarrito();
 };
 
-const eliminarDelCarrito = (prodId) => {
-    const item = carrito.find((prod) => prod.id === prodId);
-
-    const indice = carrito.indexOf(item) ;
-
-    carrito.splice(indice, 1);
-    actualizarCarrito() ;
-    console.log(carrito);
-};
-
-const actualizarCarrito = () => {
-
-    contenedorCarrito.innerHTML = ""
-    carrito.forEach((prod) => {
-        const div = document.createElement('div');
-        div.className = ('productoEnCarrito');
-        div.innerHTML = `
-        <p>${prod.nombre}</p>
-        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-        `
-
-        contenedorCarrito.appendChild(div);
-        
-        localStorage.setItem('carrito', JSON.stringify(carrito));
-
-    });
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  modalBody.innerHTML = "";
+  carrito.forEach((prod) => {
+    const { id, nombre, desc, img, cantidad } = prod;
+    console.log(modalBody);
+    modalBody.innerHTML += `
+      <div class="modal-contenedor">
+        <div>
+        <img class="img-fluid img-carrito" src="${img}"/>
+        </div>
+        <div>
+        <p>Producto: ${nombre}</p>
+      <p>Cantidad :${cantidad}</p>
+      <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+        </div>
+      </div>
+      
   
-    contadorCarrito.innerText = carrito.length 
-    cotizacionTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+      `;
+  });
+
+  if (carrito.length === 0) {
+    modalBody.innerHTML = `
+    <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+    `;
+  }
+  carritoContenedor.textContent = carrito.length;
+
+  guardarStorage();
 };
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function eliminarProducto(id) {
+  const productoId = id;
+  carrito = carrito.filter((producto) => producto.id !== productoId);
+  mostrarCarrito();
+}
